0a256b291afab60c4cbe2b28970779ac7f44305b
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-05-11 11:59:27 -0700

    push

--- main.py
+++ main.py
@@ -4,13 +4,40 @@ import random
 pygame.init()
 
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 800 #x
+WINDOW_HEIGHT = 400 #y
 
-
+score = 0
+lives = 5
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Catch the Clown!")
+
+back_music = pygame.mixer.Sound('ctc_background_music.wav')
+back_music.play(-1)
+
+back_ground = pygame.image.load("background.png")
+back_ground = pygame.transform.scale(back_ground, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+title_font = pygame.font.Font('Franxurter.ttf',23)
+score_font = pygame.font.Font('Franxurter.ttf',23)
+lives_font = pygame.font.Font('Franxurter.ttf',23)
+
+title_text = title_font.render("Catch the Clown!", True, (14,193,237))
+title_text_rect = title_text.get_rect()
+title_text_rect.topleft = (0 + 30, 0 + 10)
+
+score_text = score_font.render("Score: " + str(score), True, (239,247,5))
+score_text_rect = score_text.get_rect()
+score_text_rect.topright = (WINDOW_WIDTH - 25, 0 + 10)
+
+lives_text = lives_font.render("Lives: " + str(lives), True, (239,247,5))
+lives_text_rect = lives_text.get_rect()
+lives_text_rect.topright = (WINDOW_WIDTH - 25, 0 + 35)
+
+clown_image = pygame.image.load("clown.png")
+clown_rect = clown_image.get_rect()
+clown_rect.center = (WINDOW_WIDTH//2,WINDOW_HEIGHT//2)
 
 
 
@@ -22,7 +49,11 @@ while game_running:
         if ev.type == pygame.QUIT:
             game_running = False
 
-        
+    display_surface.blit(back_ground, (0, 0)) #背景图片
+    display_surface.blit(title_text, title_text_rect) #title
+    display_surface.blit(score_text, score_text_rect) #积分
+    display_surface.blit(lives_text, lives_text_rect) #生命
+    display_surface.blit(clown_image, clown_rect) #clown
 
     pygame.display.update()
 
